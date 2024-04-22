1e552ce993bd7528146aa50f43196f30961a81ec
Sanyog Maurya <96514393+SanyogMaurya03@users.noreply.github.com>
2024-04-22 09:40:11 +0000

    Till Conversion of DataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,58 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
+
+// <%%%%%%%%%%%%%%%%%%%%%% My Code %%%%%%%%%%%%%%%%%%%%%%%%%%>
+
+
+
+// const accountId = 75676
+// let userName = "kuch bhi"
+// var accEmail ="sanyog@gmail.com"
+//   accountCity = "Indore"     // We can also create variable like this 
+
+//   let something;  // In JS if we don't give value and try to print value of that then it will give "undefined"
+
+
+// // accountId = 2 // not allowed
+// // accountNo = 4736873   // NOT Allowed
+
+// userName = "sanyog"
+// accEmail = "something@gmail.com"
+// accountCity = "manipur"    
+
+
+//  /*
+// Prefer not to use var
+// because of issue in block scope and functional scope
+// */
+
+
+
+
+// console.log(accountId);
+
+// console.table([userName, accEmail , accountCity, something])
